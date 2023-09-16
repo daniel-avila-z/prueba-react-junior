f3b409a445dc9bf927a58833885a10bc633dcd62
Daniel Avila <roicesdaniel@gmail.com>
2023-09-16 17:36:26 -0400

    Añadiendo un skeleton loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+// import Skeleton from './components/Skeleton'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https//cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+const Skeleton = () => {
+  return (
+    <div>
+      <p className='skeleton'>Loading fact...</p>
+      <div className='card-image-container'>
+        <img className='skeleton' src={`${CAT_PREFIX_IMAGE_URL}/cat/says/Loading...`} alt='Loading image...' />
+      </div>
+    </div>
+  )
+}
 
 export function App () {
   const [fact, setFact] = useState()
@@ -38,9 +49,25 @@ export function App () {
     <main>
       <section className='card-main-container'>
         <h1>App de Gatos</h1>
+        {!fact && <Skeleton />}
         {fact && <p>{fact}</p>}
-        {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`image extracted using the first three words from cataas.com. ${fact}`} />}
+        {imageUrl &&
+          <div className='card-image-container'>
+            <img className='skeleton' src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`image extracted using the first three words from cataas.com. ${fact}`} />
+          </div>}
       </section>
     </main>
   )
+
+  // return (
+  //   <main>
+  //     <section className='card-main-container'>
+  //       <h1>App de Gatos</h1>
+  //       {fact && <p>{fact}</p>}
+  //       <div className='card-image-container'>
+  //         {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`image extracted using the first three words from cataas.com. ${fact}`} />}
+  //       </div>
+  //     </section>
+  //   </main>
+  // )
 }
